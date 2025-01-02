8c01f358237b0a056f7fee7bbdabb445ca64c150
sethstanton <sethstanton200@gmail.com>
2025-01-02 20:12:06 +0000

    getting set up
    Co-authored-by: Sophia Roscoe <SRoscoe27@users.noreply.github.com>

---
+++ src/components/UserNameInput.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserNameInput = () => {
+    return <p>hi</p>
+};
+
+export default UserNameInput;
---
+++ src/pages/Intro.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import UserNameInput from '../components/UserNameInput';
+
+/** This is going to be the login/intro page. 
+ * - as a user, I want to be able to provide the user with a url
+ * - so that they can enter their username
+ * - and be entered as a player
+ * - and are navigated to the main page to play the game
+*/
+const Intro = () => {
+    return (
+        // need username form with submit button
+        <UserNameInput/>
+
+    );
+};
+
+export default Intro;
+
+
