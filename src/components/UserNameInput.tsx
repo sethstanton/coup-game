124715072acd4daf3e807f7d60b90e383dac9dec
sethstanton <sethstanton200@gmail.com>
2025-01-03 00:54:21 +0000

    added basic username input form

--- src/components/UserNameInput.tsx
+++ src/components/UserNameInput.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UserNameInput = () => {
-    return <p>hi</p>
+    const [username, setUsername] = useState("");
+    
+    const handleSubmit = (e: React.FormEvent) =>{
+        e.preventDefault();
+        console.log("Username Submitted", username);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Enter Your User Name:
+                <input 
+                type = "text" 
+                value ={username} 
+                onChange={(e) => setUsername(e.target.value)}
+                />
+            </label>
+            <button type="submit">Submit</button>
+        </form>
+    );
 };
 
 export default UserNameInput;
